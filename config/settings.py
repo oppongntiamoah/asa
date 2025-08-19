e2215c409d75f3bc4b8e5449618ae6077469b3d9
Emmanuel Oppong Ntiamoah <ntiamoahone@gmail.com>
2025-08-19 23:16:12 +0000

    debug s

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x_5pvmq*1lvc73acg7$tzo8_j)pub_#aw18o8*v_kp8^i&e$$e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
