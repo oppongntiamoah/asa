6daf2181d24dd3b4d55996e516775efd873c9d00
Emmanuel Oppong Ntiamoah <ntiamoahone@gmail.com>
2025-08-19 23:25:34 +0000

    da

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from . models import CustomUser, AllowedUser
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import AllowedUser
+
+
+# Resource for import-export
+class AllowedUserResource(resources.ModelResource):
+    class Meta:
+        model = AllowedUser
+        fields = ("id", "email")   # specify which fields to import/export
+
+
+# Admin with import-export
+@admin.register(AllowedUser)
+class AllowedUserAdmin(ImportExportModelAdmin):
+    resource_class = AllowedUserResource
+    list_display = ("email",)
+    search_fields = ("email",)
+
 
 admin.site.register(CustomUser)
-admin.site.register(AllowedUser)
--- config/settings.py
+++ config/settings.py
@@ -86,12 +86,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'default',
+        'USER': 'agyapong',
+        'PASSWORD': 'DeadTrap1',
+        'HOST': 'agyapong.mysql.pythonanywhere-services.com',   # or IP
+        'PORT': '3306',        # default MySQL port
     }
 }
+
 
 
 # Password validation
