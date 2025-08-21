05bea2b0a0786e3acc5afb96fdc83e5bafe147d9
Emmanuel Oppong Ntiamoah <ntiamoahone@gmail.com>
2025-08-21 12:16:28 +0000

    weerdyt

--- config/settings.py
+++ config/settings.py
@@ -86,24 +86,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'default',
-#         'USER': 'agyapong',
-#         'PASSWORD': 'DeadTrap1',
-#         'HOST': '10.0.4.70',   # or IP
-#         'PORT': '3306',        # default MySQL port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'agyapong$default',
+        'USER': 'agyapong',
+        'PASSWORD': 'DeadTrap1',
+        'HOST': 'agyapong.mysql.pythonanywhere-services.com',   # or IP
+        'PORT': '3306',        # default MySQL port
+    }
+}
 
 
 
